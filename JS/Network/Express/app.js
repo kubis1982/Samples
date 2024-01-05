3901d181aff37506b2a7ba70ae428644eef14597
m.swiatnicki <m.swiatnicki@7technology.pl>
2024-01-05 14:52:15 +0100

    Komunikacja z usługą za pomocą FETCH

--- JS/Network/Express/app.js
+++ JS/Network/Express/app.js
@@ -1,14 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = 3001;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+let data1 = { name: "Święty", firstName: "Mariusz", age: 36 }
+let data2 = { name: "Barabara", firstName: "Barbara", age: 18 }
+let array = [data1, data2];
+
 app.get('/contractors', (req, res) => {
-    let data1 = { name: "Święty", firstName: "Mariusz", age: 36 }
-    let data2 = { name: "Barabara", firstName: "Barbara", age: 18 }
-    let result = { count: 2, items: [data1, data2]  }
+    let result = { count: array.length, items: array }
     res.send(result);
+});
+
+app.post('/contractors', (req, res) => {
+    const data = req.body;
+
+    console.log("Name: ", data.name);
+    console.log("Age: ", data.age);
+    console.log("FirstName: ", data.firstName);
+
+    array.push(data);
+
+    res.send();
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}! http://localhost:${port}/`));
--- JS/Network/Fetch/fetch.js
+++
@@ -1,6 +0,0 @@
-let url = new URL("http://localhost:3000/contractors");
-
-fetch(url)
-    .then(response => response.json())
-    .then(json => alert(json.items[0].name));
-
