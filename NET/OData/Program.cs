8fef340791caa1839603e71b4cdb4a6109efbf0f
m.swiatnicki <m.swiatnicki@7technology.pl>
2024-02-02 16:12:48 +0100

    Dodano EdmModel

--- NET/OData/Program.cs
+++ NET/OData/Program.cs
@@ -1,12 +1,30 @@
 using EntityFramework.Sqlite;
 using EntityFramework.Sqlite.Entities;
+using Microsoft.AspNetCore.OData;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+using System.Drawing;
+
+static IEdmModel GetEdmModel() {
+    ODataConventionModelBuilder builder = new();
+    builder.EntitySet<ExampleData>("ExampleDatas");
+    return builder.GetEdmModel();
+}
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddOData(options => options
+        .AddRouteComponents("odata", GetEdmModel())
+        .Select()
+        .Filter()
+        .OrderBy()
+        .SetMaxTop(20)
+        .Count()
+        .Expand()
+    );
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
